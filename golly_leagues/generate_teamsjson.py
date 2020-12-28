eaacaa99c889fa2ca8388e969325356e07e491e7
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2020-12-27 19:27:09 -0800

    update fields in new teams

--- golly_leagues/generate_teamsjson.py
+++ golly_leagues/generate_teamsjson.py
@@ -30,29 +30,19 @@ with open('team_nick_names.txt', 'r') as f:
     nicknames = f.readlines()
 nicknames = [line.strip() for line in nicknames]
 
-leagues = ['Hot League', 'Cold League']
-divisions = ['Fire Division', 'Water Division']
-teams_per_div = 4
-
-if len(leagues)*len(divisions)*teams_per_div > len(colors):
-    raise Exception("Error: need more colors!")
+nteams = len(colors)
 
 teams = []
 
-iC = 0
-for iL, league in enumerate(leagues):
-    for iD, division in enumerate(divisions):
-        for iT in range(teams_per_div):
-            team = {}
-            city = random.choice(cities)
-            nick = random.choice(nicknames)
-            team['teamName'] = f"{city} {nick}" 
-            team['teamAbbr'] = "".join([z[0] for z in team['teamName'].upper().split(" ")]) 
-            team['teamColor'] = colors[iC]
-            team['teamId'] = str(uuid.uuid4())
-            team['league'] = league
-            team['division'] = division
-            teams.append(team)
+for iC in range(len(colors)):
+    team = {}
+    city = random.choice(cities)
+    nick = random.choice(nicknames)
+    team['teamName'] = f"{city} {nick}" 
+    team['teamAbbr'] = "".join([z[0] for z in team['teamName'].upper().split(" ")]) 
+    team['teamColor'] = colors[iC]
+    team['teamId'] = str(uuid.uuid4())
+    teams.append(team)
 
 with open('teams.json', 'w') as f:
     json.dump(teams, f, indent=4)
